130ffb8dcbf0040a0e3010b6dab1ee0cbbd213ce
adriaanbalt <adriaan@balt.us>
2016-07-05 15:20:04 -0400

    adding post to puzzle route

--- server/routes/puzzle/index.js
+++ server/routes/puzzle/index.js
@@ -8,12 +8,17 @@ Promise.promisifyAll(Puzzle.prototype);
 
 module.exports = (router) => {
 
-	// Get initial app data
 	router.get('/puzzle', function(req, res, next) {
 		console.log ( "route api/puzzle");
 		Puzzle.findOneAsync({}, null, {})
 			.then(allPuzzles => res.json(allPuzzles))
 			.catch(err => !console.log(err) && next(err));
+	});
+
+	router.post('/puzzle', function(req, res, next) {
+		var newObj = new Puzzle(req.body);
+	    newObj.saveAsync()
+	      .then( savedObj  => res.json(savedObj) );
 	});
 
 
