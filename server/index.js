9e87cec05ea440dfd9f5a4e4da2ede213e0c0062
adriaanbalt <adriaan@balt.us>
2016-07-13 23:27:22 -0400

    adding sender info

--- server/index.js
+++ server/index.js
@@ -55,11 +55,11 @@ app.get('/webhook/', function (req, res) {
 
 // handling messages
 app.post('/webhook/', function (req, res) {
-    console.log ( 'webhook req', req );
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
+    console.log ( 'webhook sender', event, sender );
         if (event.message && event.message.text) {
             let text = "" + event.message.text.toLowerCase();
 
