87b97962a7f7519a1232af2ce5521a07ea462b07
Adriaan Scholvinck <adriaan@balt.us>
2017-06-20 13:47:17 -0400

    adding the timeout

--- server/index.js
+++ server/index.js
@@ -132,6 +132,8 @@ app.post('/webhook/', function (req, res) {
                 currentPuzzle = getPuzzle();
                 sendTextMessage(sender, "Congratulations! You have " + puzzles.length + " puzzles left to complete. Here's a new puzzle");
                 sendTextMessage(sender, currentPuzzle.pictogram );
+                let timeout = setTimeout( ()=>sendTextMessage(sender, successfulPuzzles.length + " of " + puzzles.length ), 100 )
+                clearTimeout( timeout )
             } else if ( !checkPuzzleAnswer( text ) ) {
                 sendTextMessage(sender, "Sorry that was incorrect. You have " + puzzles.length + " puzzles left to complete. Try again or respond 'new' for a different puzzle or respond 'hint' for this puzzle's hint. Reminder of your current puzzle: " + currentPuzzle.pictogram );
             }
