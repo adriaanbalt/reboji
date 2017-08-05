7aecf263e8a2819d007c38a9de277ff3261705ec
adriaanbalt <adriaan@balt.us>
2017-08-05 00:29:35 -0400

    adding active users and checking user by fbid and storing more like a session

--- server/bots/reboji.js
+++ server/bots/reboji.js
@@ -6,25 +6,25 @@
 //         puzzles: ["1468083290053", "1468083397541"],
 //         currentPuzzle: ["5432"]
 //     }
-// );
+// )
 
 // newObj.saveAsync()
 //   .then( savedObj  => {
-//     console.log ( 'response from user save:', savedObj);
-// });
+//     console.log ( 'response from user save:', savedObj)
+// })
 
 
 // User.findAsync({}, null, {})
 //     .then(allUsers => {
-//         console.log( 'ALLUSERS:', allUsers );
+//         console.log( 'ALLUSERS:', allUsers )
 //     })
-//     .catch(err => !console.log(err) && next(err));
+//     .catch(err => !console.log(err) && next(err))
 
 // User.findOne({ fbID:"1064814340266637" })
 //     .populate('currentPuzzle') // only return the Persons name
 //     .exec(function (err, story) {
-//      if (err) return handleError(err);
-//      console.log('The creator is %s', story.currentPuzzle);
+//      if (err) return handleError(err)
+//      console.log('The creator is %s', story.currentPuzzle)
 //     })
 
 // webhook sender { sender: { id: '1064814340266637' },
@@ -42,27 +42,28 @@ const mongoose = require('mongoose'),
       Promise = require('bluebird')
 
 // promisifies methods on "Puzzle" and "Puzzle" instances
-Promise.promisifyAll(Puzzle);
-Promise.promisifyAll(Puzzle.prototype);
-Promise.promisifyAll(User);
-Promise.promisifyAll(User.prototype);
+Promise.promisifyAll(Puzzle)
+Promise.promisifyAll(Puzzle.prototype)
+Promise.promisifyAll(User)
+Promise.promisifyAll(User.prototype)
 
 class Reboji {
 
     constructor(app) {
-        console.log ( "REBOJI CONSTRUCTOR" );
+        console.log ( "REBOJI CONSTRUCTOR" )
         
-        this.puzzles = [];
-        this.seenPuzzles = [];
-        this.puzzlesComplete = [];
-        this.failedPuzzles = [];
-        this.currentPuzzle;
+        this.puzzles = []
+        this.seenPuzzles = []
+        this.puzzlesComplete = []
+        this.failedPuzzles = []
+        this.activeUsers = []
+        this.currentPuzzle
         this.token = "EAAF0MuSayRkBAMi8pb5w6X2qf3rsk1wF8UCD8Nhpho0yiBknETthNd2b8o4eM0bUXZBiar1jfSlfeBJneMfSoiFjZA77gMdroLnnai7ClsjU4ZBdpFz69ZAnX2Jx1uy1WzZAc7mJbCntbQkErviZCd2obVJ7MDMfQZD"
-        this.firstMessageTime = 100;
-        this.messageDelay = 300;
-        this.facebookUserId;
-        this.currentUser;
-        this.currentUserObj;
+        this.firstMessageTime = 100
+        this.messageDelay = 300
+        this.facebookUserId
+        this.currentUser
+        this.currentUserObj
 
         // for Facebook verification
         app.get('/webhook/', (req, res) => {
@@ -75,23 +76,23 @@ class Reboji {
         // handling messages
         app.post('/webhook/',  (req, res) => {
             let messaging_events = req.body.entry[0].messaging
-            for (let i = 0; i < messaging_events.length; i++) {
+            for (let i = 0 i < messaging_events.length i++) {
                 let event = req.body.entry[0].messaging[i]
-                this.facebookUserId = event.sender.id
+                let facebookUserId = event.sender.id
                 
                 console.log ( ' ' )
 
-                if ( this.currentUser != this.facebookUserId ) {
-                    // if user doesn't exist, create a user in the database
-                    this.getUserByFbId( this.facebookUserId )
+                console.log ('sender', event.sender )
+
+                if ( !this.isUserActive( facebookUserId ) ) {
+                    this.getUserByFbId( facebookUserId )
                         .then( ( userObj )=> {
                             if ( !userObj ) {
-                                this.createUser( this.facebookUserId )
+                                // if user doesn't exist, create a user in the database
+                                this.createUser( facebookUserId )
                             } else {
-                                this.currentUserObj = userObj
-                                this.puzzlesComplete = userObj.puzzlesComplete
-                                this.currentPuzzle = userObj.currentPuzzle
-                                this.currentUser = this.facebookUserId
+                                // already exists in the DB so simply add to active user list
+                                this.addActiveUser( userObj )
                             }
                         })
                 }
@@ -100,22 +101,22 @@ class Reboji {
 
                 // if this session doesnt have a current puzzle yet, then get it off the user
                 if ( !this.currentPuzzle ) {
-                    this.getUserCurrentPuzzle()
+                    this.getUserCurrentPuzzle( facebookUserId )
                         .then( (userCurrentPuzzle) => {
                             if ( userCurrentPuzzle ) {
                                 // there is a user puzzle already
-                                this.currentPuzzle = userCurrentPuzzle
+                                  = userCurrentPuzzle
                             } else {
                                 // if there is no set user puzzle, get a random puzzle from the list
-                                this.currentPuzzle = this.getPuzzleFromList()
+                                  = this.getPuzzleFromList()
                                 // set the user's current puzzle to the randomly selected puzzle
-                                this.setUserCurrentPuzzle( this.currentPuzzle )
+                                this.setUserCurrentPuzzle(   )
                             }
-                            // console.log ( 'webhook > getUserCurrentPuzzle: ', this.currentPuzzle, ' user puzzle', userCurrentPuzzle )
-                            this.handleMessages(event, this.facebookUserId)
+                            // console.log ( 'webhook > getUserCurrentPuzzle: ',  , ' user puzzle', userCurrentPuzzle )
+                            this.handleMessages(event, facebookUserId)
                         })
                 } else {
-                    this.handleMessages(event, this.facebookUserId)
+                    this.handleMessages(event, facebookUserId)
                 }
             }
             // TODO > pair with a specific user in the database
@@ -123,13 +124,23 @@ class Reboji {
         })
     }
 
+    addActiveUser( userObj ) {
+        this.activeUsers.push( userObj )
+    }
+
+    isUserActive( userId ) {
+        let r = !!this.activeUsers.filter( activeUserObj => activeUserObj.fbId == userId )[0]
+        console.log ( 'isUserActive', r )
+        return r
+    }
+
     start() {
         Puzzle.findAsync({}, null, {})
             .then(allPuzzles => {
-                this.puzzles = allPuzzles.filter( item => item.pictogram != "" );
-                return allPuzzles;
+                this.puzzles = allPuzzles.filter( item => item.pictogram != "" )
+                return allPuzzles
             })
-            .catch(err => !console.log(err) && next(err));
+            .catch(err => !console.log(err) && next(err))
     }
 
     createUser( fbId ) {
@@ -137,11 +148,11 @@ class Reboji {
             {
                 fbID: fbId,
             }
-        );
+        )
         newObj.saveAsync()
           .then( savedObj  => {
-            console.log ( 'response from user save:', savedObj);
-        });
+            console.log ( 'response from user save:', savedObj)
+        })
     }
 
     getUserByFbId( fbId ) {
@@ -160,17 +171,17 @@ class Reboji {
     handleMessages( event ) {
 
         if (event.message && event.message.text) {
-            let text = "" + event.message.text.toLowerCase();
+            let text = "" + event.message.text.toLowerCase()
 
             // starting the game
-            if ( !this.currentPuzzle ) {
-                this.sendTextMessage( '-' );
+            if ( !  ) {
+                this.sendTextMessage( '-' )
                 setTimeout( ()=>this.sendTextMessage( "Here is your first puzzle of " + puzzles.length + " puzzles." ), this.firstMessageTime )
-                setTimeout( ()=>this.sendTextMessage( this.currentPuzzle.pictogram ), this.firstMessageTime+(this.messageDelay*1) )
+                setTimeout( ()=>this.sendTextMessage(  .pictogram ), this.firstMessageTime+(this.messageDelay*1) )
             }
             // help command
             else if ( text == "help" ) {
-                this.sendTextMessage( '-' );
+                this.sendTextMessage( '-' )
                 setTimeout( ()=>this.sendTextMessage( "Commands:" ), this.firstMessageTime )
                 setTimeout( ()=>this.sendTextMessage( "'new' : a new puzzle." ), this.firstMessageTime+(this.messageDelay*1) )
                 setTimeout( ()=>this.sendTextMessage( "'hint' : hint of the current puzzle, if there is one." ), this.firstMessageTime+(this.messageDelay*2) )
@@ -180,43 +191,43 @@ class Reboji {
             }
             // get a new puzzle
             else if ( text == "new" ) {
-                this.sendTextMessage( '-' );
+                this.sendTextMessage( '-' )
                 // update user on database to a new puzzle
                 this.setUserCurrentPuzzle( this.getPuzzleFromList() )
                 setTimeout( ()=>this.sendTextMessage( "Here is a new puzzle" ), this.firstMessageTime )
-                setTimeout( ()=>this.sendTextMessage( this.currentPuzzle.pictogram ), this.firstMessageTime+(this.messageDelay*1) )
+                setTimeout( ()=>this.sendTextMessage(  .pictogram ), this.firstMessageTime+(this.messageDelay*1) )
             }
             // get a hint
             else if ( text == "hint" ) {
                 if ( this.currentPuzzle.hint ) {
-                    this.sendTextMessage( "Here is this puzzle's hint:" );
+                    this.sendTextMessage( "Here is this puzzle's hint:" )
                     setTimeout( ()=>this.sendTextMessage( this.currentPuzzle.hint ), this.firstMessageTime+(this.messageDelay*1) )
                 } else {
-                    this.sendTextMessage( "Sorry, this puzzle does not have a hint." );
+                    this.sendTextMessage( "Sorry, this puzzle does not have a hint." )
                 }
             }
             // check your info
             else if ( text == "score" ) {
-                this.sendTextMessage( "You have completed " + this.puzzlesComplete.length + " of " + this.puzzles.length + " puzzles." );
+                this.sendTextMessage( "You have completed " + this.puzzlesComplete.length + " of " + this.puzzles.length + " puzzles." )
             }
             // get current puzzle
             else if ( text == "current" ) {
                 console.log ( 'current ', this.currentPuzzle )
-                this.sendTextMessage( "Your current puzzle is");
+                this.sendTextMessage( "Your current puzzle is")
                 setTimeout( ()=>this.sendTextMessage( this.currentPuzzle.pictogram ), this.firstMessageTime+(this.messageDelay*1) )
             }
             // get current puzzle
             else if ( text == "me" ) {
                 console.log ( 'this.currentUserObj', this.currentUserObj )
-                this.sendTextMessage( "Your user info is");
+                this.sendTextMessage( "Your user info is")
                 setTimeout( ()=>this.sendTextMessage( this.currentUserObj ), this.firstMessageTime+(this.messageDelay*1) )
             }
             // successful puzzle response
             else if ( this.checkPuzzleAnswer( text ) ) {
                 // delete a puzzle that was successfully answered
-                this.removePuzzle( this.currentPuzzle ); 
+                this.removePuzzle( this.currentPuzzle ) 
                 // add successful puzzles to a separate array for logging
-                this.updateUserPuzzlesComplete( this.currentPuzzle );
+                this.updateUserPuzzlesComplete( this.currentPuzzle )
                 // update user on database to a new puzzle
                 this.setUserCurrentPuzzle( this.getPuzzleFromList() )
 
@@ -226,7 +237,7 @@ class Reboji {
             }
             // incorrect puzzle response
             else if ( !this.checkPuzzleAnswer( text ) ) {
-                this.sendTextMessage( '-' );
+                this.sendTextMessage( '-' )
                 setTimeout( ()=>this.sendTextMessage( "Sorry that was incorrect. You have " + this.puzzlesComplete.length + " of " + this.puzzles.length + " puzzles left to complete. Try again or respond 'help' for available commands." ), 100 )
                 setTimeout( ()=>this.sendTextMessage( this.currentPuzzle.pictogram ), this.firstMessageTime+(this.messageDelay*1) )
             }
@@ -253,25 +264,24 @@ class Reboji {
         this.puzzles.filter( puzzle => puzzle === puzzleToRemove )
     }
     getPuzzle() {
-        // let returnPuzzle;
+        // let returnPuzzle
         // Puzzle.findOneAsync({}, null, {})
         //         .then(puzzle => {
-        //             returnPuzzle = puzzle;
+        //             returnPuzzle = puzzle
         //         })
-        //         .catch(err => !console.log(err) && next(err));
+        //         .catch(err => !console.log(err) && next(err))
 
-        // return returnPuzzle;
-        // let newPuzz = puzzles[ getRandom(0, puzzles.length ) ];// == currentPuzzle ? getPuzzle() : puzzles[ getRandom(0, puzzles.length ) ];
+        // return returnPuzzle
+        // let newPuzz = puzzles[ getRandom(0, puzzles.length ) ]// == currentPuzzle ? getPuzzle() : puzzles[ getRandom(0, puzzles.length ) ]
         // User.updateAsync({ fbID:this.facebookUserId }, { currentPuzzle: newPuzz._id,  })
-        return this.currentPuzzle;
+        return this.currentPuzzle
     }
 
     getPuzzleFromList() {
-        return this.puzzles[ this.getRandom(0, this.puzzles.length ) ];
+        return this.puzzles[ this.getRandom(0, this.puzzles.length ) ]
     }
 
     setUserCurrentPuzzle(puzzle) {
-        this.currentPuzzle = puzzle
         return new Promise((resolve, reject) => {
             User.findOneAsync({ fbID:this.facebookUserId })
                 .then( (userObj) => {
@@ -286,15 +296,12 @@ class Reboji {
         })
     }
 
-    getUserCurrentPuzzle() {
+    getUserCurrentPuzzle( facebookUserId ) {
         return new Promise((resolve, reject) => {
-            User.findOne({ fbID:this.facebookUserId })
+            User.findOne({ fbID:facebookUserId })
                 .populate('currentPuzzle') // only return the Persons name
                 .exec( (err, user) => {
-                    if (err) return reject(err);
-                    console.log( ' ' );
-                    console.log('getUserCurrentPuzzle() >>');
-                    console.log('user: ', user );
+                    if (err) return reject(err)
                     // currentPuzzle = story.currentPuzzle
                     resolve( user.currentPuzzle )
                 })    
@@ -304,25 +311,27 @@ class Reboji {
         //         return User.populate(userObj, {path:"currentPuzzle"})
         //     })
         //     .then( populatedObj => {
-        //         console.log ( "populatedObj!!", populatedObj );
+        //         console.log ( "populatedObj!!", populatedObj )
         //     })
-        //     .catch((err) => console.log(err));
+        //     .catch((err) => console.log(err))
     }
 
     getRandom( min, max ){
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
     checkPuzzleAnswer( text ) {
+
+        
         console.log ( 'checkPuzzleAnswer:', this.currentPuzzle, text )
         // console.log ( 'checkPuzzleAnswehr', ( this.currentPuzzle.answer.toLowerCase() == text.toLowerCase() ), text.toLowerCase(), this.currentPuzzle.answer.toLowerCase() )
-        return ( this.currentPuzzle.answer.toLowerCase() == text.toLowerCase() );
-        // for ( var i=0; i<currentPuzzle.answer.length; i++ ){
+        return ( this.currentPuzzle.answer.toLowerCase() == text.toLowerCase() )
+        // for ( var i=0 i<currentPuzzle.answer.length i++ ){
         //     if ( currentPuzzle.answer[i] == text ) {
-        //         return true;
+        //         return true
         //     }
         // }
-        // return false;
+        // return false
     }
 
     sendTextMessage( text ) {
@@ -353,7 +362,7 @@ module.exports = Reboji
 
 /*
 
-:)     :D     :(     :'(     :P     O:)     3:)     o.O     ;)     :O     -_-     >:O     :*
+:)     :D     :(     :'(     :P     O:)     3:)     o.O     )     :O     -_-     >:O     :*
 
 <3     ^_^     8-)     8|     (^^^)     :|]     >:(     :v     :/     :3     ☺     (y)     <(")
 
